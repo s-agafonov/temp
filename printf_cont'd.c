144864dbc029cafe2d2cde993d62077e47ed6b86
s-agafonov <102920128+s-agafonov@users.noreply.github.com>
2022-04-05 15:57:23 +0300

    Create printf_cont'd.c

---
+++ printf_cont'd.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	int a =3, b = 4, c = b;
+	printf("a = %d, b = %d, c = %d", a, b, c);
+        return 0;
+}                                                             								 				
